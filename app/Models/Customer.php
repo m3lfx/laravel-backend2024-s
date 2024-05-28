a34620502b62a314c6d0061ce23bd06cc0fc4ee2
rommel dalisay <rommelmdalisay@gmail.com>
2024-05-28 16:07:12 +0800

    first commit

---
+++ app/Models/Customer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+
+    use HasFactory;
+    protected $table = 'customer';
+    protected $primaryKey = 'customer_id';
+    public $timestamps = false;
+    public $fillable = [
+        'lname',
+        'fname',
+        'addressline',
+        'phone',
+        'zipcode',
+    ];
+}
