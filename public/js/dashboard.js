0bafe7902fcd386909406ec7f1f4ec9e533944db
rommel dalisay <rommelmdalisay@gmail.com>
2024-06-08 11:32:38 +0800

    pie chart

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -21,8 +21,8 @@ $(document).ready(function () {
                             'rgba(54, 162, 235, 0.2)',
                             'rgba(153, 102, 255, 0.2)',
                             'rgba(201, 203, 207, 0.2)'
-                          ],
-                          borderColor: [
+                        ],
+                        borderColor: [
                             'rgb(255, 99, 132)',
                             'rgb(255, 159, 64)',
                             'rgb(255, 205, 86)',
@@ -30,21 +30,21 @@ $(document).ready(function () {
                             'rgb(54, 162, 235)',
                             'rgb(153, 102, 255)',
                             'rgb(201, 203, 207)'
-                          ],
+                        ],
                         borderWidth: 1,
-                     
+
                     }]
                 },
                 options: {
                     scales: {
-                      y: {
-                        beginAtZero: true
-                      }
+                        y: {
+                            beginAtZero: true
+                        }
                     },
                     indexAxis: 'y',
-                  },
+                },
             });
-            
+
         },
         error: function (error) {
             console.log(error);
@@ -78,13 +78,13 @@ $(document).ready(function () {
                 },
                 options: {
                     scales: {
-                      y: {
-                        beginAtZero: true
-                      }
+                        y: {
+                            beginAtZero: true
+                        }
                     }
-                  },
+                },
             });
-            
+
         },
         error: function (error) {
             console.log(error);
@@ -99,26 +99,27 @@ $(document).ready(function () {
             console.log(data);
             var ctx = $("#itemsChart");
             var myBarChart = new Chart(ctx, {
-                type: 'pie',
+                type: 'doughnut',
                 data: {
                     labels: data.labels,
                     datasets: [{
                         label: 'number of items sold',
                         data: data.data,
+                       
                         backgroundColor: () => {
-                             //generates random colours and puts them in string
-                            
-                             var colors = [];
+                            //generates random colours and puts them in string
+
+                            var colors = [];
                             for (var i = 0; i < data.data.length; i++) {
-                              var letters = '0123456789ABCDEF'.split('');
-                              var color = '#';
-                              for (var x = 0; x < 6; x++) {
-                                color += letters[Math.floor(Math.random() * 16)];
-                              }
-                              colors.push(color);
+                                var letters = '0123456789ABCDEF'.split('');
+                                var color = '#';
+                                for (var x = 0; x < 6; x++) {
+                                    color += letters[Math.floor(Math.random() * 16)];
+                                }
+                                colors.push(color);
                             }
                             return colors;
-                          },
+                        },
                         borderColor: [
                             'rgba(75, 192, 192, 1)',
                             'rgba(255,99,132,1)'
@@ -128,9 +129,9 @@ $(document).ready(function () {
                         // hoverBackgroundColor: colors,
                     }]
                 },
-                
+
             });
-            
+
         },
         error: function (error) {
             console.log(error);
